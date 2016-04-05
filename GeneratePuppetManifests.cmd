dee8e4d1efdf19ad75f2c378c29f79a6fdbbec8a
Rob Reynolds <ferventcoder@gmail.com>
2016-04-05 13:37:49 -0500

    Do not remove the folder

--- GeneratePuppetManifests.cmd
+++ GeneratePuppetManifests.cmd
@@ -2,6 +2,4 @@
 
 SETLOCAL
 
-RD "%~dp0blueprints" /s/q > NUL
-MD "%~dp0blueprints" > NUL
 Powershell -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Bypass -Command "Import-Module '%~dp0PuppetManifestGenerator.psm1' -ErrorAction Stop; Invoke-Generator -ErrorAction Stop;"
