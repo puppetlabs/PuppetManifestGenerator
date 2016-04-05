20bfafbbe2462124a76c53bc03c3d219b1e42458
James Pogran <james.pogran@puppetlabs.com>
2016-04-05 16:26:53 -0700

    wip multiple module invocation

--- PuppetManifestGenerator.psm1
+++ PuppetManifestGenerator.psm1
@@ -20,39 +20,49 @@ Function Invoke-PuppetGenerator
   $ManifestInfo.Remove('Path') | Out-Null
   
   # slurp modules
-  Write-Verbose "Installed path: $($PSScriptRoot)"
-  $ModulePath = $PSScriptRoot
+  $ModulePath = (Join-Path $PSScriptRoot "resources")
   $Path = (Join-Path $PSScriptRoot "output")
+  Write-Verbose "Installed path: $($PSScriptRoot)"
+  Write-Verbose "Installed path: $($ModulePath)"
+  Write-Verbose "Installed path: $($path)"
 
-  [IO.FileInfo]$module = Join-Path $ModulePath "resources\users\users.ps1"
-  [IO.FileInfo]$manifestModule = Join-Path $ModulePath "resources\users\ConvertTo-ManifestsUsers.ps1"
-  
-  [string]$content = Get-Content -Path $module -Encoding UTF8
-  $code = @"
-  New-Module -ScriptBlock {$($content)} -Name $($module.BaseName) | Import-Module;
-  $($module.BaseName);
+  ls $ModulePath -file -rec | % {
+    
+    $module = $_
+
+    if($Module.Name -match "Convert"){
+      . $module
+      $jsonString = [string](Get-Content $outputFile)
+      &"ConvertTo-Manifest$($module.BaseName) -jsonString $($jsonString)"
+    }else{
+      [string]$content = Get-Content -Path $module.fullname -Encoding UTF8
+      $code = @"
+New-Module -ScriptBlock {$($content)} -Name $($module.BaseName) | Import-Module;
+$($module.BaseName);
 "@
-   $sb =[ScriptBlock]::Create($code)
-  
-  # create pssession
-  $sessions = New-PSSession @ConnectionInfo
-  
-  # invoke command
-  $CommandInfo = @{
-    Session       = $sessions
-    ThrottleLimit = 10
-    ScriptBlock   = $sb
+      $sb =[ScriptBlock]::Create($code)
+
+      # create pssession
+      $sessions = New-PSSession @ConnectionInfo
+
+      # invoke command
+      $CommandInfo = @{
+        Session       = $sessions
+        ThrottleLimit = 10
+        ScriptBlock   = $sb
+      }
+      $info = Invoke-Command @CommandInfo
+
+      # format manifest
+      if(-not(Test-path $Path)){
+        mkdir $path
+      }
+
+      $outputFile = (Join-Path $Path "$($module.BaseName).json")
+      $info | ConvertTo-JSON -Depth 10 | Out-File -Force -FilePath $outputFile
+    }
   }
-  $info = Invoke-Command @CommandInfo
-  
-  # format manifest
-  if(-not(Test-path $Path)){ mkdir $path }
-  
-  $outputFile = (Join-Path $Path "$($module.BaseName).json")
-  $info | ConvertTo-JSON -Depth 10 | Out-File -Force -FilePath $outputFile
-  
-  . $manifestModule
-  [string](Get-Content $outputFile) | ConvertTo-ManifestsUsers
+
 }
 
-Export-ModuleMember -Function *
+Export-ModuleMember -Function Invoke-PuppetGenerator
--- resources/users/Users.ps1
+++ resources/users/Users.ps1
@@ -3,7 +3,7 @@ Function Get-Users {
   param()
 
   Process {
-    Get-WMIObject -Class "Win32_UserAccount" -Filter "LocalAccount=True" | `
-      Select AccountType,Caption,Domain,SID,FullName,Name,Description
+    $props = @("AccountType","Caption","Domain","SID","FullName","Name","Description")
+    Get-WMIObject -Class "Win32_UserAccount" -Filter "LocalAccount=True" | Select $props
   }
 }
