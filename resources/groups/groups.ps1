9e3a9f550c4d5c6761407795c5434faa5fd151a2
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-04-06 14:23:14 -0700

    Updated Group Extraction

--- resources/groups/groups.ps1
+++ resources/groups/groups.ps1
@@ -1,5 +1,26 @@
+Function Get-Groups {
+  [cmdletBinding(SupportsShouldProcess=$false,ConfirmImpact='Low')]
+  param()
 
-function Get-Groups {
-
-
+  Process {
+    Get-WMIObject -Class "Win32_Group" -Filter "LocalAccount=True" | ForEach-Object -Process {
+      $props = @{
+        'SID' = $_.SID.ToString()
+        'Name' = $_.Name
+        'Description' = $_.Description
+      }
+      
+      # TODO Get members of group and append to object
+      $members = (NET LOCALGROUP "$($_.Name)" | 
+        where {$_ -AND $_ -notmatch "command completed successfully"} | 
+        select -skip 4)
+      if ($members -ne $null) {
+        $props.Members = $members
+      } else {
+        $props.Members = @()
+      }
+      
+      Write-Output (New-Object -Type PSCustomObject -ArgumentList $props)
+    }
+  }
 }
