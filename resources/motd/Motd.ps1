265b4043a5d475961f23e42dcfdcdcff081ff445
craig-gomes <craig.gomes@puppetlabs.com>
2016-04-14 16:00:44 -0700

    Clean up and null check

--- resources/motd/Motd.ps1
+++ resources/motd/Motd.ps1
@@ -6,13 +6,12 @@ Function Get-Motd {
     $regKey = "HKLM\Software\Microsoft\Windows\CurrentVersion\policies\system"
 
     $key = Get-Item -Path "Registry::$regKey"
-    
+    if($key.GetValue('legalnoticetext').ToString()){
             $props = @{
                 'Value' = $key.GetValue('legalnoticetext').ToString()
                 }
              Write-Output (New-Object -Type PSCustomObject -ArgumentList $props)
-            
         }
-     
+    }
   }
 
