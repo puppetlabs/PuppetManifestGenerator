9497cae3f6587dc27de95e1b46af8c50ef8ef3a2
Ben C <bcleverdon@coalfire.com>
2018-07-10 20:06:00 -0400

    More cleanup
    Just some more cleanup

--- resources/localgrouppolicy/ConvertTo-ManifestLocalGroupPolicy.ps1
+++ resources/localgrouppolicy/ConvertTo-ManifestLocalGroupPolicy.ps1
@@ -10,15 +10,11 @@ Function ConvertTo-ManifestLocalGroupPolicy {
 # Description
 
   Process {
-    $manifest = @"
-# This manifest is *** EXPERIMENTAL ***
-# Module code is at https://github.com/glennsarti/spike-puppet-packer-windows/tree/master/puppet/modules/windows_group_policy
+        $manifest = @"
+# Module code is at https://github.com/puppetlabs/puppetlabs-registry
 
 # Search Group Policies and find their registry information
 # http://gpsearch.azurewebsites.net/
-
-# GPUpdate is called whenever local group policy is updated
-# windows_group_policy::gpupdate { 'GPUpdate':
 # }
 
 "@
@@ -26,7 +22,6 @@ Function ConvertTo-ManifestLocalGroupPolicy {
 
     $numPolicy = 1
         $objTree | % {
-            # TODO Should change the data if it's a binary or number instead of always being a string.  Not sure if necessary
             If($($_.ValueType) -match "REG_SZ"){$type='string'}Else{$type='dword'}
             If ($($_.PolicyContext.ToLower()) -match "machine") {
                 $thisManifest = @"
